eab0f32d3e12d153b82c76f67483341590c4c377
Paweł Ziubiński <ziubi@MacBook-Pro-Pawe.local>
2019-12-14 17:25:24 +0100

    Zadanie 6.2

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,37 @@
+package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+public class TestingMain {
+    public static void main(String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+        if(calculator.add(2, 5) == 7) {
+            System.out.println("test dodawania OK");
+        } else {
+            System.out.println("Błąd dodawania!");
+        }
+
+        if(calculator.subtract(2, 5) == -3) {
+            System.out.println("test odejmowania OK");
+        } else {
+            System.out.println("Błąd odejmowania!");
+        }
+
+
+
+    }
+}
