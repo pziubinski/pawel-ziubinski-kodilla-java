bf9539673f18f6c9d4704bd26588227fd9a94976
Paweł Ziubiński <Pawel.Ziubinski@pcosa.onmicrosoft.com>
2019-12-18 15:19:56 +0100

    Zadanie 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -47,5 +48,8 @@ public class StreamMain {
         });
 
         poemBeautifier.beautify("Pięć", (a) -> a + " " + a);
+
+        System.out.println("\nUsing Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
