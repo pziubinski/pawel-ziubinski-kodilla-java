80b6e38fe00d803e05df8f3d82181b61d7c7bd2f
Paweł Ziubiński <Pawel.Ziubinski@pcosa.onmicrosoft.com>
2019-12-18 13:56:35 +0100

    Zadanie 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,51 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("jeden", (a) -> "** " + a + " **");
+        poemBeautifier.beautify("dwa", (a) -> a.toUpperCase());
+
+        poemBeautifier.beautify("trzy", (a) -> {
+            String temp = "\n";
+            for (int i = 0; i < a.length() + 6; i++) {
+                temp += "*";
+            }
+            temp += "\n** " + a + " **\n";
+            for (int i = 0; i < a.length() + 6; i++) {
+                temp += "*";
+            }
+            return temp;
+        });
+
+        poemBeautifier.beautify("CzterY", (a) -> {
+            String result = "";
+            //result = a.substring(a.length()-1 , a.length());
+
+            for (int i = 0; i < a.length(); i++) {
+                result += a.substring(a.length() - i - 1, a.length() - i);
+            }
+            return result;
+        });
+
+        poemBeautifier.beautify("Pięć", (a) -> a + " " + a);
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,8 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+    public void beautify(String str, PoemDecorator poemDecorator) {
+        String result = poemDecorator.decorate(str);
+        System.out.println("Result: " + result);
+    }
+}
