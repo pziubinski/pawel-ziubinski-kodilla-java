c284a9a10d997a9bb21c6268e5c472b4061782fa
pziubinski <p.ziubinski@gmail.com>
2020-03-11 14:48:21 +0100

    Zadanie 15.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+    @Test
+    public void testOneMessage() {
+        //Given
+        Logger.getInstance().log("New text in log.");
+
+        //When
+
+        //Then
+        Assert.assertEquals("New text in log.", Logger.getInstance().getLastLog());
+    }
+
+    @Test
+    public void testOverwritedMessage() {
+        //Given
+        Logger.getInstance().log("New text in log.");
+
+        //When
+        Logger.getInstance().log("Second text in log.");
+
+        //Then
+        Assert.assertEquals("Second text in log.", Logger.getInstance().getLastLog());
+    }
+
+}
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,7 +1,6 @@
 package com.kodilla.spring.portfolio;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
