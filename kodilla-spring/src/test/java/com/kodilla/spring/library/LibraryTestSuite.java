74b2bb2d53e4563327dccf7a0ec9d18a6fe288f0
pziubinski <p.ziubinski@gmail.com>
2020-03-11 12:48:24 +0100

    Zadanie 14.3 i 14.4

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -7,9 +7,23 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LibraryTestSuite {
+    @Test
+    public void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(LibraryConfig.class);
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
+    }
 
     @Test
     public void testLoadFromDb() {
@@ -36,5 +50,4 @@ public class LibraryTestSuite {
         //Then
         //do nothing
     }
-
 }
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BoardTestSuite {
+    @Test
+    public void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+
+        //When
+        Board board = context.getBean(Board.class);
+        board.getToDoList().getTasks().add("do");
+        board.getInProgressList().getTasks().add("progress");
+        board.getDoneList().getTasks().add("done");
+
+        //Then
+        board.readTask();
+    }
+
+    @Test
+    public void testTaskAddList() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+
+        //When
+        Board board = context.getBean(Board.class);
+        board.getToDoList().addTasks("do");
+        board.getInProgressList().addTasks("progress");
+        board.getDoneList().addTasks("done");
+
+        //Then
+        board.readTask();
+    }
+}
