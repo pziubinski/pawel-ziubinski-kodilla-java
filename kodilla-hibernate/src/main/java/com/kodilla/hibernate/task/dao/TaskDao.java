a80769ef6701c837703bc756830b2516b4473828
pziubinski <p.ziubinski@gmail.com>
2020-03-23 14:01:28 +0100

    Zadanie 17.4.1

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -3,13 +3,24 @@ package com.kodilla.hibernate.task.dao;
 import com.kodilla.hibernate.task.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import javax.transaction.Transactional;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 @Transactional
 @Repository
 public interface TaskDao extends CrudRepository<Task, Integer> {
     List<Task> findByDuration(int duration);
+
+    @Query
+    List<Task> retrieveLongTasks();
+
+    @Query
+    List<Task> retrieveShortTasks();
+
+    @Query(nativeQuery = true)
+    List<Task> retrieveTasksWithEnoughTime();
+
+
 
 }
