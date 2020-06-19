98ba852ce7ac82e254867460034c511728f65408
pziubinski <p.ziubinski@gmail.com>
2020-06-19 09:38:05 +0200

    Zadanie 24.1.1

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FacadeTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FacadeTestSuite.java
@@ -4,9 +4,7 @@ import com.kodilla.hibernate.manytomany.Company;
 import com.kodilla.hibernate.manytomany.Employee;
 import com.kodilla.hibernate.manytomany.dao.CompanyDao;
 import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +25,70 @@ public class FacadeTestSuite {
 
     @Autowired
     private EmployeeDao employeeDao;
+
+    @Test
+    public void findByPartOfNameFacade() throws SearchingProcessingException {
+        //Given
+        Employee johnSmithson = new Employee("John", "Smithson");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmithson);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmithson);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmithson.getCompanies().add(softwareMachine);
+        johnSmithson.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        employeeDao.save(johnSmithson);
+        int johnId = johnSmithson.getId();
+        employeeDao.save(stephanieClarckson);
+        int stephanieId = stephanieClarckson.getId();
+        employeeDao.save(lindaKovalsky);
+        int lindaId = lindaKovalsky.getId();
+
+        List<Employee> employeeList = searchingFacade.searchingEmployee("son");
+        System.out.println("##########");
+        System.out.println(employeeList);
+
+        List<Company> companyList = searchingFacade.searchingCompany("ter");
+        System.out.println("##########");
+        System.out.println(companyList);
+        System.out.println("##########");
+
+        //Then
+        Assert.assertEquals(2, employeeList.size());
+        Assert.assertEquals(2, companyList.size());
+
+        //CleanUp
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+            employeeDao.deleteById(johnId);
+            employeeDao.deleteById(stephanieId);
+            employeeDao.deleteById(lindaId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
 
     @Test
     public void findByPartOfName() throws SearchingProcessingException {
@@ -66,17 +128,17 @@ public class FacadeTestSuite {
         employeeDao.save(lindaKovalsky);
         int lindaId = lindaKovalsky.getId();
 
-        List<Employee> employeeSmith = employeeDao.retrievePartOfLastName("Smith");
+        List<Employee> employeeList = employeeDao.retrievePartOfLastName("Smith");
         System.out.println("##########");
-        System.out.println(employeeSmith);
-        List<Company> companySoftware = companyDao.retrievePartOfLastName("Software");
+        System.out.println(employeeList);
+        List<Company> companyList = companyDao.retrievePartOfLastName("Software");
         System.out.println("##########");
-        System.out.println(companySoftware);
+        System.out.println(companyList);
         System.out.println("##########");
 
         //Then
-        Assert.assertEquals(1, employeeSmith.size());
-        Assert.assertEquals(1, companySoftware.size());
+        Assert.assertEquals(1, employeeList.size());
+        Assert.assertEquals(1, companyList.size());
 
         //CleanUp
         try {
@@ -90,6 +152,4 @@ public class FacadeTestSuite {
             //do nothing
         }
     }
-
-
 }
